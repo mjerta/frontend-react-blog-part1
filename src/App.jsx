37a1d6301161d8f5ef8f89db67d6ab3bbd1e6bfe
Maarten Postma <maarten.postma1000@gmail.com>
2024-09-28 13:07:42 +0200

    Add navigation component with nav links and styling

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,20 @@ import Home from "./pages/Home.jsx";
 import NewPost from "./pages/NewPost.jsx";
 import NotFound from "./pages/NotFound.jsx";
 import OverView from "./pages/OverView.jsx";
+import Navigation from "./components/Navigation.jsx";
+import logoNavbar from "./assets/logo-medium.png";
 
 function App() {
   return (
     <>
+      <Navigation
+        img={logoNavbar}
+      />
       <Routes>
-        <Route path="/" element={<Home/>}/>
+        <Route path={"/"} element={<Home/>}/>
         <Route path={"/newpost"} element={<NewPost/>}/>
         <Route path={"/overview"} element={<OverView/>}/>
-        <Route path="/404" element={<NotFound/>}/>
+        <Route path={"/404"} element={<NotFound/>}/>
         {/*<div className="page-container">*/}
         {/*  <img src={logo} alt="Company logo"/>*/}
         {/*  <h1>Begin hier met het maken van jouw blog-applicatie!</h1>*/}
